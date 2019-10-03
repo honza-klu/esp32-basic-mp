3707edf1d31983da11e7148d7ea108dfbdc2b3a2
unknown <honza.klu@gmail.com>
2019-10-03 23:02:21 +0200

    Upgraded micropython version

--- flash_firmware.bat
+++ flash_firmware.bat
@@ -1,2 +1,2 @@
 python -m esptool --port %ESP_PORT% erase_flash
-python -m esptool --port %ESP_PORT% --baud 460800 write_flash --flash_size=detect 0x1000 ./src/other/firmware.bin
+python -m esptool --port %ESP_PORT% --baud 460800 write_flash --flash_size=detect 0x1000 ./src/other/esp32-20180511-v1.9.4.bin
